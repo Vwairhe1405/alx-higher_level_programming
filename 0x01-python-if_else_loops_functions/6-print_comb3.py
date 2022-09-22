173bcbd350223d01b9c8e123fe88750e834e674f
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-09-22 14:24:38 -0700

    Print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 99):
-    if (i % 10 > i / 10):
-        if (i == 1):
-            print("{:02d}".format(i), end="")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if (i != 8):
+            print('{}{}'.format(i, j), end=', ')
         else:
-            print(", {:02d}".format(i), end="")
-            print()
+            print('{}{}'.format(i, j))
