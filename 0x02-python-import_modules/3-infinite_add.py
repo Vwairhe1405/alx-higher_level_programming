20c1fd1549098c9b23e2035e6b31e93c444241ac
Vwairhe1405 <vwairheokolosi@gmail.com>
2022-09-22 17:17:06 -0700

    Import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-# Author: JDRZ
+import sys
 
-
-if __name__ == "__main__":
-    import sys
-    sum = 0
-    if len(sys.argv) is 1:
-        print("{}".format(sum))
-    elif len(sys.argv) > 1:
-        for x in range(1, len(sys.argv)):
-            sum = sum + int(sys.argv[x])
-        print("{}".format(sum))
+if __name__ == '__main__':
+    args = sys.argv
+    args.pop(0)
+    print(sum(map(int, args)))
